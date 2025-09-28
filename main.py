b8362ebdc9c0ec8192e7bd86e371cbe4de693352
Milind Jamnekar <milindjamnekar502@gmail.com>
2025-09-28 19:43:34 +0530

    Add CircleShape class for game object representation

--- main.py
+++ main.py
@@ -4,17 +4,30 @@
 import pygame
 from constants import *
 
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, radius, color):
+        super().__init__()
+        self.radius = radius
+        self.color = color
+        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, color, (radius, radius), radius)
+        self.rect = self.image.get_rect()
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print(dt)
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
