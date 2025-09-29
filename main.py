bb09109d4646446e5af551df25b7fb3144d235a4
Milind Jamnekar <milindjamnekar502@gmail.com>
2025-09-29 08:24:52 +0530

    Add AsteroidField and Asteroid classes for asteroid management

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
+from asteroidfield import AsteroidField
 from constants import *
+
+from astroid import Asteroid
 from player import Player
 
 def main():
@@ -14,17 +18,35 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+
         updatable.update(dt)
-        for sprite in drawable:
-            sprite.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                return
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
         
 
